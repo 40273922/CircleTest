1bb65ae121e0ffc68b7020d024355bb6c12133b9
“goodboy” <88391769+40273922@users.noreply.github.com>
2022-11-04 09:58:38 +0800

    Initial commit2 !

--- src/Sqrt.java
+++ src/Sqrt.java
@@ -7,13 +7,12 @@
  * <p>
  * &#064;create: -11-03 02:59
  */
-public class Sqrt {
-    public static void main(String[] args) {
-    }
-    public static double sqrt(double A ){
-        double Xo = 1;
+public class Sqrt{
+    public static void main(String[]args){}
+    public static double sqrt(double A){
+        double Xo=1;
         do{
-            Xo = Xo-(Xo*Xo-A)/(2*Xo);
+            Xo=Xo-(Xo*Xo-A)/(2*Xo);
         }while (!((Xo*Xo-A)<1E-323));
         return Xo;
     }
